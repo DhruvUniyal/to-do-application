c791117d136663b9a1b68592da516c259571e631
DhruvUniyal <dhruvuniyal.12.04@gmail.com>
2023-05-04 18:20:03 +0530

    Initial message

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import React, { useState } from "react";
+// import { Textfit } from 'react-textfit';
+import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/js/bootstrap.bundle.min";
+import Button from 'react-bootstrap/Button'
+import Form from 'react-bootstrap/Form';
+import Card from 'react-bootstrap/Card'
+import { Container } from "react-bootstrap";
 
 function App() {
+  const[taskList, setTaskList] = useState([{task: ""}]);
+
+  const handleClick = () => {
+    setTaskList([...taskList, {task: ""}])
+    {console.log(taskList)}
+  };
+
+  const handleChange = (e, index) => {
+    const {name,value} = e.target;
+    const list = [...taskList];
+    list[index][name] = value;
+    setTaskList(list);
+  };
+
+  const handleRemove = (index) => {
+    const list = [...taskList];
+    list.splice(index,1);
+    setTaskList(list);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className = 'note-wrap'>
+          {taskList.map((taskname,index) => (
+            <Container>
+              <Card className='mb-3'>
+                <Card.Body max={16}>{taskList[index].task}</Card.Body>
+                {taskList.length > 1 && (<Button variant='danger' type="button" onClick={() => handleRemove(index)}>Remove Note</Button>)}
+              </Card>
+            </Container>
+          ))}
+      </div>
+      {taskList.map((taskname,index) => (
+        <div>
+        {taskList.length - 1 === index && (
+          <div className = 'add-note'>
+            <Container fluid>
+              <Form>
+                <Form.Group>
+                  <Form.Control type="text" id="text-box" name="task" value={taskname.task} onChange={(e) => handleChange(e,index)} />
+                  <Button id = "submit-btn" onClick={(e) => handleClick(e,index)}>Add note</Button>
+                </Form.Group>
+              </Form>
+            </Container>
+          </div>
+          )
+        }
+        </div>
+      ))
+    }
     </div>
   );
 }
